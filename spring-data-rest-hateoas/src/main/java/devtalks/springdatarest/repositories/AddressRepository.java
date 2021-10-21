63013e611f8eeff1a0b168d4883eb9219733533e
ctudose <44235992+ctudose@users.noreply.github.com>
2021-10-21 12:57:25 +0300

    Delete spring-data-rest-hateoas directory

--- spring-data-rest-hateoas/src/main/java/devtalks/springdatarest/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package devtalks.springdatarest.repositories;
-
-import devtalks.springdatarest.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
