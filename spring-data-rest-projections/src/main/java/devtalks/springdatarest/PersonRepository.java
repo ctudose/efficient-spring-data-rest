1d08aa83095b8ff642e7fcf36b9660666656c65b
ctudose <44235992+ctudose@users.noreply.github.com>
2021-10-21 12:57:39 +0300

    Delete spring-data-rest-projections directory

--- spring-data-rest-projections/src/main/java/devtalks/springdatarest/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package devtalks.springdatarest;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(excerptProjection = PersonProjection.class)
-public interface PersonRepository extends CrudRepository<Person, Long> {
-
-}
