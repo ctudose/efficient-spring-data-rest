d164708356e8c027aa54b1ffb4b13c2abdd9f2dd
ctudose <44235992+ctudose@users.noreply.github.com>
2021-10-21 12:58:08 +0300

    Delete spring-data-rest directory

--- spring-data-rest/src/main/java/devtalks/springdatarest/exceptions/PersonNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package devtalks.springdatarest.exceptions;
-
-public class PersonNotFoundException extends RuntimeException {
-
-    public PersonNotFoundException(Long id) {
-        super("Person id not found : " + id);
-    }
-
-}
--- spring-data-rest/src/main/java/devtalks/springdatarest/model/CountryRepository.java
+++
@@ -1,6 +0,0 @@
-package devtalks.springdatarest.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
